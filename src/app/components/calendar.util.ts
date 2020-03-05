f9b823a3d6f50aec48effb3e4cb0acb1dfa811e1
kagol <kagol@sina.com>
2020-03-05 22:38:57 +0800

    refactor: remove useless code

--- src/app/components/calendar.util.ts
+++ src/app/components/calendar.util.ts
@@ -1,14 +1,6 @@
 import * as moment from 'moment';
 import { DEFAULT_ROW_NUMBER, DEFAULT_DATE_FORMAT } from './calendar.config';
 
-export function generateArray(number) {
-    const result = [];
-    for(let item = 0; item < number; item++) {
-        result.push(item);
-    }
-    return result;
-}
-
 export function getCompleteDateRange(date) {
     let [ first, second ] = date;
     const today = new Date();
@@ -64,7 +56,6 @@ export function getDateArr(dateRange) {
         firstWeekNum,
         columnNum 
     } = calculateColumnNumber(dateRange);
-    console.log('columnNum:', columnNum);
     const start = moment(dateRange[0]).add(firstWeekNum, 'days').format(DEFAULT_DATE_FORMAT);
     const dateArr = [];
     for (let rowIndex = 0; rowIndex < DEFAULT_ROW_NUMBER; rowIndex++) {
@@ -78,63 +69,4 @@ export function getDateArr(dateRange) {
         dateArr.push(columns);
     }
     return dateArr;
-}
-
-/**
- * 获取某一周的周数组
- * @param {某个日期} date 
- * @param {第几周} num 
- * @param {数组的值的格式} format 
- */
-export function getWeekArr(date, num, showOtherMonth) {
-    let weekArr = [];
-    let dateMoment = date;
-    if(!moment.isMoment(date)){
-        dateMoment = moment(date);
-    }
-    const thisMonth = dateMoment.format('M');
-    let index = dateMoment.format('d');
-    let firstDay = -index+num*7;
-    let lastDay = 7-index+num*7;
-    for(let day=firstDay;day<lastDay;day++){
-        let weekItem = moment(dateMoment).add(day, 'days');
-        let formatWeekItem = weekItem;
-        if(!showOtherMonth && thisMonth !== formatWeekItem.format('M')){
-            formatWeekItem = null;
-        }
-        weekArr.push(formatWeekItem);
-    }
-    return weekArr;
-}
-
-/**
- * 获取一个月的第一天
- * @param {某个日期} date 
- */
-export function getFirstDayOfMonth(date, monthNum, yearNum) {
-    let dateMoment = date;
-    if(!moment.isMoment(date)){
-        dateMoment = moment(date);
-    }
-    let year = moment(dateMoment).add(yearNum, 'years').add(monthNum, 'months').format('YYYY');
-    let month = moment(dateMoment).add(yearNum, 'years').add(monthNum, 'months').format('MM');
-    // let month = dateMoment.format('MM');
-    let day = '01';
-    let firstDayOfMonth = year+'-'+month+'-'+day;
-    return firstDayOfMonth;
-}
-
-/**
- * 获取日历数组
- * @param {某个日期} date 
- */
-export function getCalendarArr(date, monthNum=0, yearNum=0, showOtherMonth=true) {
-    const DATE_ROW_COUNT = 6;
-    let calendarArr = [];
-    let firstDayOfMonth = getFirstDayOfMonth(date, monthNum, yearNum);
-    for(let row=0;row<DATE_ROW_COUNT;row++){
-        let rowArr = getWeekArr(firstDayOfMonth, row, showOtherMonth);
-        calendarArr.push(rowArr);
-    }
-    return calendarArr;
 }
