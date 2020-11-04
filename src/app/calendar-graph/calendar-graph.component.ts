506cf41bdc111dc552c9cd92cc451ffec5633b2b
kagol <kagol@sina.com>
2020-11-04 22:37:10 +0800

    feat: 增加schedule API，用于显示提交日程

--- src/app/calendar-graph/calendar-graph.component.ts
+++ src/app/calendar-graph/calendar-graph.component.ts
@@ -20,6 +20,7 @@ export class CalendarGraphComponent implements OnInit {
   dateArr;
   legendColor = LEGEND_COLOR;
   colorData;
+  completeDateRange;
 
   ngOnInit() {
       let [ start, end ] = this.getDateRange(this.date);
@@ -29,9 +30,10 @@ export class CalendarGraphComponent implements OnInit {
       }
       
       const dateRange = getCompleteDateRange([ start, end ]);
+      this.completeDateRange = dateRange;
       
       const { columnNum } = calculateColumnNumber(dateRange);
-      this.dateArr = getDateArr(dateRange);
+      this.dateArr = getDateArr(dateRange); // TODO: 日期数组计算有误，将导致渲染出来的日期错误
       if (!this.sourceData) {
           if (this.text) {
               this.sourceData = transpose(textToSymbolArray(this.text));
@@ -45,6 +47,10 @@ export class CalendarGraphComponent implements OnInit {
       }
 
       this.colorData = this.sourceData;
+
+      if (this.schedule) {
+        this.showSchedule();
+      }
   }
 
   getDateRange(date) {
@@ -68,6 +74,19 @@ export class CalendarGraphComponent implements OnInit {
   getEndDate(start, text) {
     const dateColumn = textToSymbolArray(text); // 计算列数（周数）
     return moment(start).add(dateColumn.length, 'weeks');
+  }
+
+  showSchedule() {
+    const symbolArray = textToSymbolArray(this.text);
+    const numberArray = symbolArray.reduce((a, b) => a.concat(b), []);
+    const [ start ] = this.completeDateRange;
+    for (const index in numberArray) {
+      const date = moment(start).add(index, 'days').format(DEFAULT_DATE_FORMAT);
+      const number = numberArray[index];
+      if (number > 0) {
+        console.log(`您需要在${date}提交${number}次`);
+      }
+    }
   }
 
 }
