d2ff6f757c4b79971db333b8e89036b14c67819a
kagol <kagol@sina.com>
2020-12-01 23:19:00 +0800

    feat: 删除src下的calendar-graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { CalendarGraphModule } from './calendar-graph/calendar-graph.module';
-// import { CalendarGraphModule } from '../../dist/calendar';
-import { CalendarGraphModule } from 'ng-calendar-graph';
+// import { CalendarGraphModule } from './calendar-graph/calendar-graph.module'; // 使用未打包的模块
+// import { CalendarGraphModule } from '../../dist/calendar'; // 使用打包后的模块
+// import { CalendarGraphModule } from 'ng-calendar-graph'; // 使用npm库
+import { CalendarGraphModule } from 'projects/calendar/src/lib/calendar-graph.module'; // 从library引入模块
 
 @NgModule({
   declarations: [
